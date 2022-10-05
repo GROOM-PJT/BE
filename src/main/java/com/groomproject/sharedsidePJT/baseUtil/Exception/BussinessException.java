d4f0649bc0545f99b6e23945913a83d25d7bd177
Jeeseob <jeeseob5761@gmail.com>
2022-10-05 15:40:32 +0900

    baseUtil - test controller 삭제 + BussinessException 수정

--- src/main/java/com/groomproject/sharedsidePJT/baseUtil/Exception/BussinessException.java
+++ src/main/java/com/groomproject/sharedsidePJT/baseUtil/Exception/BussinessException.java
@@ -5,8 +5,24 @@ package com.groomproject.sharedsidePJT.baseUtil.Exception;
  * @CreateAt : 2022/10/04
  */
 
-public class BussinessException extends RuntimeException{
+public class BussinessException extends RuntimeException {
+    public BussinessException() {
+        super();
+    }
+
     public BussinessException(ExMessage exMessage) {
         super(exMessage.getMessage());
+    }
+
+    public BussinessException(String message) {
+        super(message);
+    }
+
+    public BussinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BussinessException(Throwable cause) {
+        super(cause);
     }
 }
